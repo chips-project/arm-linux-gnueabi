a5202461f7326908fbd581913671fbb5b4fa7f9f
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-06-06 11:20:22 +0700

    arm-linux-gnueabi-gcc (GCC) 11.0.0 20200605 (experimental)
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

---
+++ lib/gcc/arm-linux-gnueabi/11.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/najahi/workspace/.build/arm-linux-gnueabi/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=arm-linux-gnueabi --prefix=/home/najahi/x-tools/arm-linux-gnueabi --with-sysroot=/home/najahi/x-tools/arm-linux-gnueabi/arm-linux-gnueabi/sysroot --enable-languages=c,c++ --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/najahi/workspace/.build/arm-linux-gnueabi/buildtools --with-mpfr=/home/najahi/workspace/.build/arm-linux-gnueabi/buildtools --with-mpc=/home/najahi/workspace/.build/arm-linux-gnueabi/buildtools --with-isl=/home/najahi/workspace/.build/arm-linux-gnueabi/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/najahi/x-tools/arm-linux-gnueabi/arm-linux-gnueabi/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
